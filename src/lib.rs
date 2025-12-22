8e44c3cf8f7d81bd8ac347db256d1a1b5f0166ce
Parker Timmerman <parker@parkertimmerman.com>
2025-12-22 09:39:26 -0500

    delete the *Buffer traits, they're not needed

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,5 @@ mod error;
 mod util;
 pub mod wire;
 
-// Publically export `buffer` so users can implement the relevant traits.
-pub mod buffer;
 // Publically export `leb128` because the functions are useful on their own.
 pub mod leb128;
