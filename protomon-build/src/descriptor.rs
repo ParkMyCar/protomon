53cadf90b4ff5de4927a7ffcee55cbf9346b195e
Parker Timmerman <parker@parkertimmerman.com>
2025-12-23 15:03:04 -0500

    protomon-build, refactor to use protomon-derive instead of hand rolled serialization for the descriptor types

--- protomon-build/src/descriptor.rs
+++ protomon-build/src/descriptor.rs
@@ -3,131 +3,167 @@
 //! These types mirror google/protobuf/descriptor.proto but are implemented
 //! independently to avoid depending on prost.
 
-mod decode;
+use crate::Error;
+use protomon::codec::ProtoMessage;
+use protomon::ProtoMessage as ProtoMessageDerive;
 
-pub use decode::decode_file_descriptor_set;
+/// Decode a FileDescriptorSet from protobuf binary data.
+pub fn decode_file_descriptor_set(data: &[u8]) -> Result<FileDescriptorSet, Error> {
+    FileDescriptorSet::decode_message(bytes::Bytes::from(data.to_vec()))
+        .map_err(|e| Error::DecodeError(e.to_string()))
+}
 
 /// A collection of file descriptors.
 /// Corresponds to google.protobuf.FileDescriptorSet.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct FileDescriptorSet {
     /// The file descriptors.
-    pub file: Vec<FileDescriptorProto>, // field 1
+    #[proto(tag = 1, repeated)]
+    pub file: Vec<FileDescriptorProto>,
 }
 
 /// Describes a complete .proto file.
 /// Corresponds to google.protobuf.FileDescriptorProto.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct FileDescriptorProto {
     /// The file name, relative to root of source tree.
-    pub name: Option<String>, // field 1
+    #[proto(tag = 1, optional)]
+    pub name: Option<String>,
     /// The package name.
-    pub package: Option<String>, // field 2
+    #[proto(tag = 2, optional)]
+    pub package: Option<String>,
     /// Names of files imported by this file.
-    pub dependency: Vec<String>, // field 3
+    #[proto(tag = 3, repeated)]
+    pub dependency: Vec<String>,
     /// All top-level message definitions in this file.
-    pub message_type: Vec<DescriptorProto>, // field 4
+    #[proto(tag = 4, repeated)]
+    pub message_type: Vec<DescriptorProto>,
     /// All top-level enum definitions in this file.
-    pub enum_type: Vec<EnumDescriptorProto>, // field 5
+    #[proto(tag = 5, repeated)]
+    pub enum_type: Vec<EnumDescriptorProto>,
     /// The syntax of the proto file (e.g., "proto2", "proto3").
-    pub syntax: Option<String>, // field 12
+    #[proto(tag = 12, optional)]
+    pub syntax: Option<String>,
 }
 
 /// Describes a message type.
 /// Corresponds to google.protobuf.DescriptorProto.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct DescriptorProto {
     /// The message name.
-    pub name: Option<String>, // field 1
+    #[proto(tag = 1, optional)]
+    pub name: Option<String>,
     /// Fields of the message.
-    pub field: Vec<FieldDescriptorProto>, // field 2
+    #[proto(tag = 2, repeated)]
+    pub field: Vec<FieldDescriptorProto>,
     /// Nested message types.
-    pub nested_type: Vec<DescriptorProto>, // field 3
+    #[proto(tag = 3, repeated)]
+    pub nested_type: Vec<DescriptorProto>,
     /// Nested enum types.
-    pub enum_type: Vec<EnumDescriptorProto>, // field 4
-    /// Oneof declarations.
-    pub oneof_decl: Vec<OneofDescriptorProto>, // field 8
+    #[proto(tag = 4, repeated)]
+    pub enum_type: Vec<EnumDescriptorProto>,
     /// Message options.
-    pub options: Option<MessageOptions>, // field 7
+    #[proto(tag = 7, optional)]
+    pub options: Option<MessageOptions>,
+    /// Oneof declarations.
+    #[proto(tag = 8, repeated)]
+    pub oneof_decl: Vec<OneofDescriptorProto>,
 }
 
 /// Describes a field within a message.
 /// Corresponds to google.protobuf.FieldDescriptorProto.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct FieldDescriptorProto {
     /// The field name.
-    pub name: Option<String>, // field 1
+    #[proto(tag = 1, optional)]
+    pub name: Option<String>,
     /// The field number (tag).
-    pub number: Option<i32>, // field 3
+    #[proto(tag = 3, optional)]
+    pub number: Option<i32>,
     /// The field label (optional, required, repeated).
-    pub label: Option<i32>, // field 4
+    #[proto(tag = 4, optional)]
+    pub label: Option<i32>,
     /// The field type.
-    pub r#type: Option<i32>, // field 5
+    #[proto(tag = 5, optional)]
+    pub r#type: Option<i32>,
     /// For message and enum types, the fully-qualified type name.
-    pub type_name: Option<String>, // field 6
+    #[proto(tag = 6, optional)]
+    pub type_name: Option<String>,
     /// The default value as a string.
-    pub default_value: Option<String>, // field 7
+    #[proto(tag = 7, optional)]
+    pub default_value: Option<String>,
     /// Field options (includes protomon extensions).
-    pub options: Option<FieldOptions>, // field 8
+    #[proto(tag = 8, optional)]
+    pub options: Option<FieldOptions>,
     /// If set, this field is part of a oneof.
-    pub oneof_index: Option<i32>, // field 9
+    #[proto(tag = 9, optional)]
+    pub oneof_index: Option<i32>,
     /// The JSON name for this field.
-    pub json_name: Option<String>, // field 10
+    #[proto(tag = 10, optional)]
+    pub json_name: Option<String>,
     /// If true, this is a proto3 optional field.
-    pub proto3_optional: Option<bool>, // field 17
+    #[proto(tag = 17, optional)]
+    pub proto3_optional: Option<bool>,
 }
 
 impl FieldDescriptorProto {
     /// Get the field label.
     pub fn label(&self) -> Label {
-        self.label.map(Label::from_i32).flatten().unwrap_or(Label::Optional)
+        self.label.and_then(Label::from_i32).unwrap_or(Label::Optional)
     }
 
     /// Get the field type.
     pub fn field_type(&self) -> Option<Type> {
-        self.r#type.map(Type::from_i32).flatten()
+        self.r#type.and_then(Type::from_i32)
     }
 }
 
 /// Describes an enum type.
 /// Corresponds to google.protobuf.EnumDescriptorProto.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct EnumDescriptorProto {
     /// The enum name.
-    pub name: Option<String>, // field 1
+    #[proto(tag = 1, optional)]
+    pub name: Option<String>,
     /// The enum values.
-    pub value: Vec<EnumValueDescriptorProto>, // field 2
+    #[proto(tag = 2, repeated)]
+    pub value: Vec<EnumValueDescriptorProto>,
 }
 
 /// Describes an enum value.
 /// Corresponds to google.protobuf.EnumValueDescriptorProto.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct EnumValueDescriptorProto {
     /// The value name.
-    pub name: Option<String>, // field 1
+    #[proto(tag = 1, optional)]
+    pub name: Option<String>,
     /// The value number.
-    pub number: Option<i32>, // field 2
+    #[proto(tag = 2, optional)]
+    pub number: Option<i32>,
 }
 
 /// Describes a oneof.
 /// Corresponds to google.protobuf.OneofDescriptorProto.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct OneofDescriptorProto {
     /// The oneof name.
-    pub name: Option<String>, // field 1
+    #[proto(tag = 1, optional)]
+    pub name: Option<String>,
     /// Oneof options (includes protomon extensions).
-    pub options: Option<OneofOptions>, // field 2
+    #[proto(tag = 2, optional)]
+    pub options: Option<OneofOptions>,
 }
 
 /// Options for a oneof.
 /// Corresponds to google.protobuf.OneofOptions with protomon extensions.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct OneofOptions {
     // Protomon extensions (field numbers 50000-50049 reserved for oneof options)
 
     /// Whether the oneof is nullable (wrapped in `Option<T>`).
     /// Default is true. When false, decoding fails if oneof is missing.
     /// Extension field 50000.
+    #[proto(tag = 50000, optional)]
     pub nullable: Option<bool>,
 }
 
@@ -141,32 +177,37 @@ impl OneofOptions {
 
 /// Options for a message type.
 /// Corresponds to google.protobuf.MessageOptions.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct MessageOptions {
     /// Set true if this message is a map entry type.
-    pub map_entry: Option<bool>, // field 7
+    #[proto(tag = 7, optional)]
+    pub map_entry: Option<bool>,
 }
 
 /// Options for a field.
 /// Corresponds to google.protobuf.FieldOptions with protomon extensions.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, ProtoMessageDerive)]
 pub struct FieldOptions {
     // Protomon extensions (field numbers 50001-50099)
 
     /// Use `Vec<T>` instead of `Repeated<T>` for repeated fields.
     /// Extension field 50001.
+    #[proto(tag = 50001)]
     pub vec: bool,
 
     /// Wrap field type in `Box<T>`.
     /// Extension field 50002.
+    #[proto(tag = 50002)]
     pub boxed: bool,
 
     /// Wrap message field in `LazyMessage<T>` for lazy/zero-copy decoding.
     /// Extension field 50003.
+    #[proto(tag = 50003)]
     pub lazy: bool,
 
     /// Use fixed-size array `[u8; N]` instead of `ProtoBytes` for bytes fields.
     /// Extension field 50004. Value of 0 means not set.
+    #[proto(tag = 50004)]
     pub fixed_array: u32,
 }
 
